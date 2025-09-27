1f69eb224e39c87d6aa8e7fdc7df42ce1e2e5356
hanxala <hanzalakhan0912@gmail.com>
2025-09-28 02:46:23 +0530

    chore(next): ignore ESLint errors during production build

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'lh3.googleusercontent.com', pathname: '/**' },
     ],
   },
+  eslint: {
+    // Allow production builds to successfully complete even if there are ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
